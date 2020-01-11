62299865e2350ad81c8b76576fe02adc8ed082b2
fengqianan <45323984+fengqianan@users.noreply.github.com>
2020-01-11 13:12:46 +0800

    Add files via upload

---
+++ 905_按奇偶排序数组.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+
+using namespace std;
+
+//两次遍历
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+        vector<int> ans;        
+        for (auto itr=A.begin(); itr!=A.end(); itr++) 
+            if (*itr%2 == 0) 
+                ans.push_back(*itr);               
+        for (auto itr=A.begin(); itr!=A.end(); itr++) 
+            if (*itr%2 != 0) 
+                ans.push_back(*itr);
+        return ans;
+    }
+};
+
+
+//原地算法，双指针
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+        int i=0, j=A.size()-1;
+        while(j>=i) {
+            if (A[i]%2 == 0)
+                i++;
+            if (A[j]%2 == 1)
+                j--;
+            if (A[i]%2==1 && A[j]%2==0) {
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+        return A;
+    }
+};
